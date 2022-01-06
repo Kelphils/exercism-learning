8f70d04b136dfed7108e5074a7d3183c21b99886
Obioha Chibueze Kelvin <53217748+Kelphils@users.noreply.github.com>
2022-01-06 10:52:07 +0100

    Add files via upload

---
+++ Exercism/Python/Exercism lasagna test.py
@@ -0,0 +1,45 @@
+EXPECTED_BAKE_TIME = 40
+
+PREPARATION_TIME = 2
+
+
+def bake_time_remaining(elapsed_bake_time):
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int baking time already elapsed.
+    :return: int remaining bake time derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+    result_2 = EXPECTED_BAKE_TIME - elapsed_bake_time
+    return result_2
+
+
+def preparation_time_in_minutes(exp_time):
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int baking time already elapsed.
+    :return: int remaining bake time derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+    result = PREPARATION_TIME * exp_time
+    return result
+
+
+def elapsed_time_in_minutes(elapsed, prep):
+    """Calculate the bake time remaining.
+
+    :param elapsed_bake_time: int baking time already elapsed.
+    :return: int remaining bake time derived from 'EXPECTED_BAKE_TIME'.
+
+    Function that takes the actual minutes the lasagna has been in the oven as
+    an argument and returns how many minutes the lasagna still needs to bake
+    based on the `EXPECTED_BAKE_TIME`.
+    """
+    fn_elapsed = elapsed * 2
+    return fn_elapsed + prep
